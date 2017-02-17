2125e398f12e7421895501968b9e9988be1b61d9
Alexander Seeliger <alexander.seeliger@alexosoft.de>
2017-02-17 09:48:26 +0100

    Switched to HeuristicNet representation for graph generation Added arc count Added start and end nodes

--- src/de/tudarmstadt/tk/pm/graph/ConvertToGraphMLPlugin.java
+++ src/de/tudarmstadt/tk/pm/graph/ConvertToGraphMLPlugin.java
@@ -1,15 +1,14 @@
 /*
- * Copyright (c) 2016 TK - Telecooperation Lab
+ * Copyright (c) 2016-2017 TK - Telecooperation Lab
  *
- * This file is part of prom-GraphMLExporter.
+ * This file is part of ProM GraphMLExporter.
  * 
- * prom-GraphMLExporter is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * ProM GraphMLExporter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
  * 
- * You should have received a copy of the GNU General Public License
- * along with prom-GraphMLExporter.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * ProM GraphMLExporter. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 package de.tudarmstadt.tk.pm.graph;
@@ -22,20 +21,25 @@ import java.util.Map;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import org.deckfour.xes.classification.XEventClass;
 import org.processmining.contexts.uitopia.UIPluginContext;
 import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
 import org.processmining.framework.plugin.annotations.Plugin;
 import org.processmining.framework.util.ui.widgets.helper.UserCancelledException;
 import org.processmining.models.heuristics.HeuristicsNet;
-import org.processmining.models.heuristics.HeuristicsNetGraph;
-import org.processmining.models.heuristics.elements.HNEdge;
-import org.processmining.models.heuristics.elements.HNNode;
+import org.processmining.models.heuristics.impl.HNSet;
+import org.processmining.models.heuristics.impl.HNSubSet;
 
+import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 import com.tinkerpop.blueprints.util.io.graphml.GraphMLWriter;
 
 public class ConvertToGraphMLPlugin {
+
+	private static final String PROPERTY_ACTIVITY = "Activity";
+	
+	private static final String PROPERTY_COUNT = "Count";
 
 	@Plugin(name = "Export to GraphML format (GXL)", parameterLabels = {
 			"HeuristicNet" }, help = "Converts a HeuristicNet into a GraphML format.", userAccessible = true, returnLabels = {}, returnTypes = {}, mostSignificantResult = -1)
@@ -45,7 +49,7 @@ public class ConvertToGraphMLPlugin {
 		// show file chooser
 		JFileChooser fc = new JFileChooser();
 		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		fc.setFileFilter(new FileNameExtensionFilter("GraphML format (.gxl)", "gxl"));
+		fc.setFileFilter(new FileNameExtensionFilter("GraphML format (.graphml)", "graphml"));
 
 		int result = fc.showSaveDialog(null);
 		if (result == JFileChooser.CANCEL_OPTION) {
@@ -53,26 +57,66 @@ public class ConvertToGraphMLPlugin {
 		}
 
 		File outputFile = fc.getSelectedFile();
+		exportToFile2(net, outputFile);
 
-		// convert heuristics net into graph
-		HeuristicsNetGraph heuristicGraph = new HeuristicsNetGraph(net, "", false);
+		context.getProgress().setMaximum(100);
+		
+	}
+
+	private void exportToFile2(HeuristicsNet net, File outputFile) throws IOException {
+
+		XEventClass[] events = net.getActivitiesMappingStructures().getActivitiesMapping();
+
 		TinkerGraph graph = new TinkerGraph();
 
-		// convert nodes
-		Map<HNNode, Vertex> nodeToBlueVertex = new LinkedHashMap<>();
-		
-		for (HNNode node : heuristicGraph.getNodes()) {
-			Vertex vertex = graph.addVertex(node.getId().toString());
-			nodeToBlueVertex.put(node, vertex);
-			
-			vertex.setProperty("activity", node.getAttributeMap().get("ProM_Vis_attr_label"));
+		// add start and end node
+		Vertex startVertex = graph.addVertex("START");
+		startVertex.setProperty(PROPERTY_ACTIVITY, "START");
+
+		Vertex endVertex = graph.addVertex("END");
+		endVertex.setProperty(PROPERTY_ACTIVITY, "END");
+
+		// add nodes
+		Map<Integer, Vertex> nodeToBlueVertex = new LinkedHashMap<>();
+
+		for (XEventClass event : events) {
+			Vertex vertex = graph.addVertex(event.getIndex());
+			vertex.setProperty(PROPERTY_ACTIVITY, event.getId());
+
+			nodeToBlueVertex.put(event.getIndex(), vertex);
+
+			if (net.getStartActivities().contains(event.getIndex())) {
+				graph.addEdge("START->" + event.getIndex(), startVertex, vertex, "START->" + event.getIndex());
+			}
+
+			if (net.getEndActivities().contains(event.getIndex())) {
+				graph.addEdge(event.getIndex() + "->END", vertex, endVertex, event.getIndex() + "->END");
+			}
 		}
-		
-		for (HNEdge<? extends HNNode, ? extends HNNode> edge : heuristicGraph.getEdges()) {
-			graph.addEdge(getId(edge), 
-					nodeToBlueVertex.get(edge.getSource()), 
-					nodeToBlueVertex.get(edge.getTarget()),
-					getId(edge));
+
+		// add outgoing edges
+		for (XEventClass event : events) {
+			HNSet sets = net.getOutputSet(event.getIndex());
+
+			for (int i = 0; i < sets.size(); i++) {
+				HNSubSet subset = sets.get(i);
+
+				for (int j = 0; j < subset.size(); j++) {
+					try {
+						
+						Edge edge = graph.addEdge(
+								event.getIndex() + "->" + subset.get(j), 
+								nodeToBlueVertex.get(event.getIndex()),
+								nodeToBlueVertex.get(subset.get(j)), 
+								event.getIndex() + "->" + subset.get(j));
+						
+						edge.setProperty(PROPERTY_COUNT, net.getArcUsage().get(event.getIndex(), subset.get(j)));
+						
+					} catch (Exception ex) {
+						// no idea why edges occur multiple times
+					}
+				}
+			}
 		}
 
 		// export graph
@@ -80,10 +124,6 @@ public class ConvertToGraphMLPlugin {
 		writer.setNormalize(true);
 		writer.outputGraph(outputFile.getAbsolutePath());
 
-		context.getProgress().setMaximum(100);
-
 	}
-	private static String getId(HNEdge<? extends HNNode, ? extends HNNode> edge) {
-		return edge.getSource().getId().toString() + "->" + edge.getTarget().getId().toString();
-	}
+	
 }
